c20108248497d8e2e99186570e45fbaeece66a06
cap <vncapit@gmail.com>
2023-01-06 17:56:22 +0800

    Weather vue component 1

---
+++ src/components/WeatherApi.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+       <div class="header">
+            <h1 class="app-name">
+                
+            </h1>
+            <p class="app-desc">
+                
+            </p>
+       </div> 
+       <div class="weather">
+        
+       </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
