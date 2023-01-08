b3a46fd86dbd13dbd42253d862ebc2de0fc81c29
unknown <vncapit@gmail.com>
2023-01-08 22:39:09 +0800

    ok call api using axios

--- src/components/WeatherApi.vue
+++ src/components/WeatherApi.vue
@@ -9,17 +9,21 @@
             </p>
         </div>
         <select v-model="weathers" multiple="true" class="city-list">
-            <option class="city-list-item" v-for="city in cities" v-bind:key="city">
+            <option v-for="city in cities" v-bind:key="city">
                 {{ city }}
             </option>
         </select>
         <div class="weathers">
-            <div v-for="city in cities" :key="city">
+            <div class="weather" v-for="cityInfo in weatherInfos" :key="cityInfo.city">
                 <div class="city">
-                    {{ city }}
+                    {{ cityInfo.city }}
                 </div>
                 <div class="condition-text">
-                    {}
+                    {{cityInfo.info?.current?.condition?.text }}
+                </div>
+                <img class="condition-icon" :src="cityInfo.info?.current?.condition?.icon" alt="">
+                <div class="temperature">
+                    {{ cityInfo.info?.current?.temp_c }}
                 </div>
             </div>
         </div>
@@ -37,43 +41,119 @@ export default {
         return {
             cities: ['Ha Noi', 'Ha Tinh', 'Can Loc', 'Ky Anh', 'Vinh', 'Manila', 'Pasay'],
             weathers: [],
+            weatherInfos: [],
         }
     },
     mounted() {
-
+    
+    },
+    watch: {
+        weathers(newValue) {
+            this.weatherInfos = []
+            newValue.forEach(city => {
+                this.weatherInfos.push({
+                    city: city,
+                    info: {}
+                })
+                this.getWeatherData(city)
+            });
+            console.log(this.weatherInfos)
+        }
     },
     methods: {
-        getWeatherData(city) {
+        async getWeatherData(city) {
             let api = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
-            let data = axios.get(api).then(response => {
+            let data = await axios.get(api).then(response => {
                 console.log(response.data)
+                this.setCityInfo(city, response.data)
                 return response.data
             }).catch(error => {
                 console.log(error)
                 return false
             })
             return data
+        },
+
+        setCityInfo(city, data) {
+            let index = this.weathers.indexOf(city)
+            this.weatherInfos[index].info = data
         }
     },
 }
 </script>
 
 <style lang="scss" scoped>
+* {
+    margin: 0;
+    padding: 0;
+    color: #fff;
+}
 .container {
     display: flex;
     flex-direction: column;
     justify-items: center;
+    background-color: rgb(92,64,132);
+    color: #fff;
 }
 
 .city-list {
     display: block;
     width: 150px;
-    height: 25px;
+    height: 28px;
     text-align: left;
-
-    .city-list-item {
+    overflow: hidden;
+    border: 1px solid;
+    
+    option {
         display: block;
         padding: 5px ;
     }
+
+    option:nth-child(2n - 1) {
+        background-color: rgb(151, 237, 243);
+    }
+    option:nth-child(2n) {
+        background-color: rgb(238, 241, 194);
+    }
+
+    &:hover {
+        height: 150px;
+        overflow: auto;
+    }
 }
+.weathers {
+    display: flex;
+    flex-direction: row;
+}
+
+.weather {
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(0, 0, 0, 0);
+    width: 280px;
+    height: 210px;
+    border: 2px solid;
+    .city {
+        font-size: 24px;
+        font-weight: 500;
+        margin-top: 20px;
+    }
+    .condition-text {
+        font-size: 12px;
+        font-weight: 200;
+        color: rgba(255, 255, 255, 0.6) ;
+        padding: 2px;
+    }
+    .condition-icon {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 80px;
+        height: auto;
+    }
+    .temperature {
+        font-size: 22px;
+    }
+}
+
 </style>
