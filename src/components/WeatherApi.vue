d76e77abd565e4233c854547b7725260b3a18810
cap <vncapit@gmail.com>
2023-01-08 19:37:08 +0800

    Weather App 2

--- src/components/WeatherApi.vue
+++ src/components/WeatherApi.vue
@@ -2,12 +2,17 @@
     <div class="container">
         <div class="header">
             <h1 class="app-name">
-
+                Vue Weather App
             </h1>
             <p class="app-desc">
-
+                Build atop Vue & OpenWeatherMap
             </p>
         </div>
+        <select v-model="weathers" multiple="true" class="city-list">
+            <option class="city-list-item" v-for="city in cities" v-bind:key="city">
+                {{ city }}
+            </option>
+        </select>
         <div class="weathers">
             <div v-for="city in cities" :key="city">
                 <div class="city">
@@ -31,25 +36,44 @@ export default {
     data() {
         return {
             cities: ['Ha Noi', 'Ha Tinh', 'Can Loc', 'Ky Anh', 'Vinh', 'Manila', 'Pasay'],
+            weathers: [],
         }
     },
-    mounted () {
-        this.getWeatherData(this.cities[1]);
+    mounted() {
+
     },
     methods: {
         getWeatherData(city) {
             let api = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
-            axios.get(api).then( response => {
+            let data = axios.get(api).then(response => {
                 console.log(response.data)
                 return response.data
-            }).catch( error => {
+            }).catch(error => {
                 console.log(error)
+                return false
             })
+            return data
         }
     },
 }
 </script>
 
 <style lang="scss" scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-items: center;
+}
 
+.city-list {
+    display: block;
+    width: 150px;
+    height: 25px;
+    text-align: left;
+
+    .city-list-item {
+        display: block;
+        padding: 5px ;
+    }
+}
 </style>
