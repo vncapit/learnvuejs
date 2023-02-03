26e750a7b91bb6868fa72dd5f3af309804ff43c6
cap <vncapit@gmail.com>
2023-02-03 17:57:29 +0800

    Vue Preloader 1

---
+++ src/components/PreLoader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="preloader">
+            <div class="loading"></div>
+            <div class="done"></div>
+        </div>
+        <div class="main">
+            <p>Nice to meet you, i'm a title!</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'PreLoader',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
