8c68c18a316e66f0d4c62b344757da0cda948e3b
cap <vncapit@gmail.com>
2023-02-04 18:02:10 +0800

    Vue Preloader 3

--- src/components/PreLoader.vue
+++ src/components/PreLoader.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="container">
-        <div class="preloader"   :class="{'hide-preloader' : (percent == 100)}">
-            <div>I'm {{ percent }} % done!</div>
-            <div class="crazy">Let get crazy!</div>
-            <div class="progress-bar"></div>
+        <div class="preloader" :class="{ 'loader-done-1': done }">
+            <div v-if="percent != 101" class="percent">I'm {{ percent }} % done!</div>
+            <div v-if="percent == 101">Let get crazy!</div>
+            <div v-if="percent != 101" class="progress-bar" :style="`left: ${percent - 100}%;`"></div>
         </div>
-        <div class="main" >
+        <div class="main" :class="{ 'loader-done-2': done }">
             <p>Nice to meet you, i'm a title!</p>
         </div>
-
     </div>
 </template>
 
@@ -19,6 +18,7 @@ export default {
         return {
             percent: 0,
             timer: null,
+            done: false,
         }
     },
     mounted() {
@@ -28,10 +28,13 @@ export default {
         counter() {
             this.timer = setInterval(() => {
                 this.percent++;
-                if (this.percent == 100) {
+                if (this.percent == 101) {
                     clearInterval(this.timer);
+                    setTimeout(() => {
+                        this.done = true;
+                    }, 1500);
                 }
-            }, 20)
+            }, 40)
         }
     },
 }
@@ -47,29 +50,65 @@ export default {
 .container {
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(60deg, rgb(236, 213, 118), rgb(210, 241, 84));
+    overflow: hidden;
+    position: relative;
 
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 36px;
+    font-weight: 600;
+    color: #fff;
 
     .preloader {
+        background-color: #3498DB;
         width: 100%;
-        height: 110vh;
-        background-color: aqua;
+        height: 100vh;
+        position: absolute;
+        top: 0;
 
         display: flex;
         flex-direction: column;
-        align-items: center;
         justify-content: center;
+        align-items: center;
 
-        transition: all 3s linear;
+        transition: all 2s linear;
+
+        .percent {
+
+        }
+
+        .progress-bar {
+            position: absolute;
+            top: 55%;
+            left: -100%;
+            width: 100%;
+            height: 4px;
+            background-color: #fff;
+        }
+
     }
 
-    .hide-preloader {
-        transform: translateY(-110vh);
+    .loader-done-1 {
+        top: -100%;
+    }
+
+    .loader-done-2 {
+        top: 0% !important;
     }
 
     .main {
-        background-color: blueviolet;
+        background-color: #34495E;
         width: 100%;
-        height: 110vh;
+        height: 100vh;
+        position: absolute;
+        top: 100%;
+        transition: all 2s linear;
+        padding-top: 20px;
+
     }
 }
 </style> 
